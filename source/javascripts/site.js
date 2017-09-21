b7d26b797c819fc54903c1b6eaad5dfb772e68c3
Andrew Bash <andrew@andrewbash.com>
2017-09-20 20:24:08 -0400

    Adding javascript parlaying to header

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -1,1 +1,18 @@
-// This is where it all goes :)
+'use strict'
+
+window.onload = function() {
+ paralaxHeader();   
+};
+
+function paralaxHeader() {
+    const header = document.querySelector('.site__header');
+    const body = document.querySelector('.site__body');
+    let paralaxOffset = header.offsetHeight;
+
+    header.classList.add('site__header--fixed');
+    body.style.marginTop = paralaxOffset + 'px';
+}
+
+
+
+
